38022a677b9bddfd6a6a06f8844c34097306205f
Deepak Gopal <deepakgopal@VYMO-IT-LPT-632.local>
2025-08-18 22:33:12 +0530

    Getting better with cypress

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ id = #idNmae / tagName#className
 class = .className / tagName.className
 others = tagname[] eg:tagname[type='search']
 Parents = parentTagName childTagName
+
+Cypress being a node project is asynchronos in nature, but cypress uses .then() function to make it synchronos
--- cypressAutomation/cypress/e2e/basicTest.cy.js
+++ cypressAutomation/cypress/e2e/basicTest.cy.js
@@ -1,5 +1,26 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+/// <reference types="cypress" />
+
+describe('Basic Bofore Test', () => {
+
+  it('login to vymo', () => {
+    cy.fixture('users').then((user) => {
+      cy.visit(user.stagingUrl).wait(6000)
+      cy.get('#field-username').should('exist').type(user.validUserId);
+      cy.get('button[data-test-id="login-button"]').should('exist').click();
+      cy.get('#field-password').should('exist').type(user.validUserPassword);
+      cy.get('button[data-test-id="password-button"]').should('exist').click();
+      cy.url().should('include', '/dashboard');
+      cy.get('.ant-menu').find('.ant-menu-submenu').as('menu');
+      cy.get('@menu').should('have.length.gt', 4);
+      cy.get('@menu').each(($el) => {
+        let role = $el.attr('role');
+        if(role !== 'menuitem') { // is not a menu item
+           let x = $el.find('div').attr('role');
+           assert.equal('menuitem', x, 'The div insite the list view should be a menuItem'); // should be a menu item
+        } else {
+          cy.log('This is a menu item');
+        }
+      });
+    })
   })
-})
+});
